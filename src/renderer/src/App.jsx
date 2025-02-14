0e7abd8ba84387c72c0afb396f8ad191ab547a47
Mega Nugraha <monmega110@gmail.com>
2025-02-15 01:15:18 +0700

    adding custom fonts

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,10 +1,10 @@
-function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+import '@fontsource/iosevka-aile'
 
+function App() {
   return (
-    <div className="flex flex-col bg-macchiato-mantle w-full min-h-screen overflow-auto">
-      <div className="flex flex-col m-auto p-3 shadow-md bg-red-600 items-center justify-center">
-        <p className="p-2 text-3xl text-blue-600">Hello World</p>
+    <div className="font-display flex flex-col bg-macchiato-mantle w-full min-h-screen overflow-auto">
+      <div className="flex flex-col m-auto p-3 shadow-md items-center justify-center">
+        <p className="p-2 text-3xl text-blue-600 ">Hello World</p>
       </div>
     </div>
   )
